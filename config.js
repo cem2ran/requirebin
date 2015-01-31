5ef8cc3913ee4e91a657ed763403e28c72f407d0
Cem Turan <cem2ran@gmail.com>
2015-01-31 16:21:01 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     GITHUB_CLIENT: '77f2992b68fc4f9d04f3',
-    GATEKEEPER: 'https://ce.ms/gatekeeper',
+    GATEKEEPER: 'https://requirebin-gatekeeper.herokuapp.com',
     BROWSERIFYCDN: 'http://browserify.ce.ms'
 }
